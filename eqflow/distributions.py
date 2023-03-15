604303325ee1af3261927866a89f093400612c40
Jeff Shen <shenjeff@princeton.edu>
2023-03-15 00:45:53 -0400

    separate eqdistributions

---
+++ eqflow/distributions.py
@@ -0,0 +1,27 @@
+import equinox as eqx
+from jaxtyping import PyTree
+from typing import Any
+
+
+class EqDistribution(eqx.Module):
+    cls: type
+    args: PyTree[Any]
+    kwargs: PyTree[Any]
+
+    def __init__(self, cls, *args, **kwargs):
+        self.cls = cls
+        self.args = args
+        self.kwargs = kwargs
+
+    def log_prob(self, x):
+        return self.cls(*self.args, **self.kwargs).log_prob(x)
+
+    def sample(self, key, sample_shape):
+        return self.cls(*self.args, **self.kwargs).sample(
+            seed=key, sample_shape=sample_shape
+        )
+
+    def sample_and_log_prob(self, key, sample_shape):
+        return self.cls(*self.args, **self.kwargs).sample_and_log_prob(
+            seed=key, sample_shape=sample_shape
+        )
